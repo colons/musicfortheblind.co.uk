4329b8f7242722a41a3428c8d8a469f9ce41d63c
Iain Dawson <iain@bldm.us>
2013-11-16 15:37:18 +0000

    Pull IndexView out of the music app.

--- mftb5/apps/music/urls.py
+++ mftb5/apps/music/urls.py
@@ -1,7 +1,5 @@
-from django.conf.urls import patterns, url
-from mftb5.apps.music.views import IndexView
+from django.conf.urls import patterns  # , url
 
 urlpatterns = patterns(
     '',
-    url('^$', IndexView.as_view(), name='index')
 )
--- mftb5/urls.py
+++ mftb5/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from mftb5.views import IndexView
+
+
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^', include('mftb5.apps.music.urls')),
+    url('^$', IndexView.as_view(), name='index'),
+    url(r'^', include('mftb5.apps.music.urls', namespace='music')),
     url(r'^admin', include(admin.site.urls)),
 )
