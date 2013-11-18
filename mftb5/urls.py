be76bcbbff9501e2ec75f5472a1bd1bdc74b0686
Iain Dawson <iain@bldm.us>
2013-11-18 19:10:05 +0000

    Redirect from /favicon.ico.

--- mftb5/urls.py
+++ mftb5/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 from mftb5.views import IndexView
 
@@ -9,6 +10,9 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    url('^favicon.ico$',
+        RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico')),
+
     url('^$', IndexView.as_view(), name='index'),
     url(r'^news/', include('mftb5.apps.news.urls', namespace='news')),
     url(r'^admin/', include(admin.site.urls)),
