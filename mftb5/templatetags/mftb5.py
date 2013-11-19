b91ce3e6e270a542121b8afe703c8cbc9b22d343
Iain Dawson <iain@bldm.us>
2013-11-19 20:06:11 +0000

    Better explain what external links on projects are for.

--- mftb5/templatetags/mftb5.py
+++ mftb5/templatetags/mftb5.py
@@ -8,4 +8,13 @@ register = Library()
 @register.filter
 def service_from_url(url):
     host = urlparse(url).netloc
-    return host
+
+    if host.startswith('www.'):
+        host = host[4:]
+
+    return {
+        'soundcloud.com': 'listen on SoundCloud',
+        'nivi.bandcamp.com': 'download on Bandcamp',
+        'globalgamejam.org': 'see the game on The Global Game Jam',
+        'moddb.com': 'see the game on Mod DB',
+    }.get(host, host)
