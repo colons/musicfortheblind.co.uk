903dbd866a9b9553fc0e8d70002e8f484727fc9e
Iain Dawson <iain@bldm.us>
2014-01-19 16:49:47 +0000

    Improve track admin.

--- mftb5/apps/music/admin.py
+++ mftb5/apps/music/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from mftb5.apps.music.models import Album, Track
 
-for model in [Album, Track]:
-    admin.site.register(model)
+
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ('name', 'album', 'track_number')
+    list_filter = ('album', 'published')
+    ordering = ('album', 'track_number')
+
+
+admin.site.register(Album)
+admin.site.register(Track, TrackAdmin)
