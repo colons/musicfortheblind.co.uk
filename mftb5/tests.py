0057fc5859ce7ee48206c8a6a34e4c04c332ba92
Iain Dawson <iain@colons.co>
2014-03-13 21:42:00 +0000

    Test more things. Instantly.

--- mftb5/tests.py
+++ mftb5/tests.py
@@ -1,26 +1,41 @@
 from django.test import TestCase
 
-from instant_coverage import InstantCoverageMixin
+from instant_coverage import InstantCoverageMixin, optional
 
 
-class EverythingTest(InstantCoverageMixin, TestCase):
+from mftb5.apps.music.models import Track, Album
+
+covered_urls = {
+    '/',
+    '/meta/',
+    '/contact/',
+    '/about/',
+    '/news/',
+    '/news/blade-teardown/',
+    '/music/',
+    '/albums/',
+    '/playlist/',
+    '/music/dust/',
+    '/projects/thrust/',
+    '/orphans/',
+    '/pyrrha/pyrrha/',
+}
+
+
+for model in [Track, Album]:
+    for instance in model.objects.all():
+        covered_urls.add(instance.get_absolute_url())
+
+
+class EverythingTest(
+    optional.ValidHTML5,
+    optional.ValidJSON,
+    InstantCoverageMixin, TestCase
+):
     fixtures = ['music', 'news']
+    spelling_language = 'en_GB'
 
-    covered_urls = [
-        '/',
-        '/meta/',
-        '/contact/',
-        '/about/',
-        '/news/',
-        '/news/blade-teardown/',
-        '/music/',
-        '/albums/',
-        '/playlist/',
-        '/music/dust/',
-        '/projects/thrust/',
-        '/orphans/',
-        '/pyrrha/pyrrha/',
-    ]
+    covered_urls = list(covered_urls)
     uncovered_urls = [
         '/favicon.ico',
     ]
